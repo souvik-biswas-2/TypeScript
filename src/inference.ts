0c5a814b32b8e8f226d8e0ce96da2389b6f5439b
Souvik Biswas <souvik-biswas-2>
2025-12-14 18:33:08 +0530

    Annotations

---
+++ src/inference.ts
@@ -0,0 +1,13 @@
+// let num : number = 15;
+
+// over annotation is not needed
+// let num : string = 15;
+
+let num = 15;
+// num = '15';
+
+
+// annotation needed
+export function add(num1 : number, num2 : number) : number{
+    return num1 + num2;
+}
